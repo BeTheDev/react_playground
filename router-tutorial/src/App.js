a23a31f5b1290f02180b7e2547473dbc9a44fb6f
daonez <cyongchoi@gmail.com>
2020-06-17 00:23:05 +0900

    finish router

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Home from "./Home"
+import About from "./About.js"
+import { Route, Link, Switch } from "react-router-dom"
+import HistorySample from "./HistorySample"
+import Profiles from "./Profiles"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/history">History</Link>
+        </li>
+        <li>
+          <Link to="/profiles"> 프로필</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/about", "info"]} component={About} />
+        <Route path="/history" component={HistorySample} />
+        <Route path="/profiles" component={Profiles} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
